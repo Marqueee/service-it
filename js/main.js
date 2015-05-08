688768b2aeb9572cc55d4ae790aa3d01506e4edf
Marqueee <virusmoto@gmail.com>
2015-05-08 11:05:22 +0500

    test_commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+$(function () {
+  $('input, textarea').placeholder();
+});
+$(".rotate").textrotator({
+  animation: "flipUp",
+  separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+  speed: 2000 // How many milliseconds until the next word show.
+});
+(function($){
+			$(window).load(function(){
+				
+				/* 
+				get snap amount programmatically or just set it directly (e.g. "273") 
+				in this example, the snap amount is list item's (li) outer-width (width+margins)
+				*/
+				var amount=Math.max.apply(Math,$(".slider li").map(function(){return $(this).outerWidth(true);}).get());
+				
+				$(".slider").mCustomScrollbar({
+					axis:"x",
+					theme:"dark-3",
+                    scrollButtons:{ enable: true },
+					advanced:{
+						autoExpandHorizontalScroll:true},
+                    scrollbarPosition: "outside",
+					mouseWheel:{scrollAmount:1000}
+				});
+				
+			});
+		})(jQuery);
+$( "#car" ).hover(
+  function() {
+    $( this ).append( $( "<span> ***</span>" ) );
+  }, function() {
+    $( this ).find( "span:last" ).remove();
+  }
+);
